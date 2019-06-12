35d856118067b43584bcffbb4bd8cea53c342b0f
Tim Kye <tyrsius@gmail.com>
2019-06-11 19:06:06 -0700

    fix docs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Client } from './lib/Client'
-export { Base } from './lib/Base'
-export { Table } from './lib/Table'
+export * from './lib/Client'
+export * from './lib/Base'
+export * from './lib/Table'
 export { createQuery } from './lib/query'
